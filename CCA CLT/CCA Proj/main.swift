873cf979e596b8ae1978ef321b01396ed95d6198
CC <dsy1998@live.com>
2016-10-07 18:35:23 -0400

    Add preliminary support for command line arguments. Change output file format to ".png". The command line arguments in order are: threshold maxNeighborDistance dimension iterationCount [outputFileName]. The last one is optional; if it's not supplied, then the default value of "Output.png" will be used. The value of the last argument or its default value will be appended to the current working directory to create the path for the output file.
    The dimension of the output image will be the dimension of the CA's grid.

--- CCA CLT/CCA Proj/main.swift
+++ CCA CLT/CCA Proj/main.swift
@@ -8,24 +8,53 @@
 
 import Cocoa
 
-print("Starting universe...")
+let colorMapping = [PixelData(red: 249, green: 67, blue: 145), PixelData(red: 0, green: 243, blue: 162), PixelData(red: 244, green: 169, blue: 0), PixelData(red: 156, green: 168, blue: 68), PixelData(red: 142, green: 143, blue: 144), PixelData(red: 64, green: 143, blue: 249), PixelData(red: 220, green: 242, blue: 0), PixelData(red: 112, green: 23, blue: 247)]
 
-var universe = Universe(maxStateValue: 8, threshold: 5, neighborDistance: 2, width: 1000, height: 1000)
-
-for i in 0 ..< 1000 {
-    print("Iteration: \(i)")
-    universe.iterate()
+func main() {
+    guard CommandLine.arguments.count >= 5 else {
+        print("Missing \(5 - CommandLine.arguments.count) argument(s).")
+        return
+    }
+    
+    let baseUrl = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+    let fileName = CommandLine.arguments.count >= 6 ? CommandLine.arguments[5] : "Output.png"
+    let outputUrl = URL(fileURLWithPath: fileName, relativeTo: baseUrl)
+    
+    print("Starting universe...")
+    
+    guard let threshold = Int(CommandLine.arguments[1]) else {
+        print("Argument not a number")
+        return
+    }
+    guard let neighborDistance = Int(CommandLine.arguments[2]) else {
+        print("Argument not a number")
+        return
+    }
+    guard let dimension = Int(CommandLine.arguments[3]) else {
+        print("Argument not a number")
+        return
+    }
+    guard let iterationCount = Int(CommandLine.arguments[4]) else {
+        print("Argument not a number")
+        return
+    }
+    
+    var universe = Universe(maxStateValue: 8, threshold: threshold, neighborDistance: neighborDistance, width: dimension, height: dimension)
+    
+    for i in 0 ..< iterationCount {
+        print("Iteration: \(i)")
+        universe.iterate()
+    }
+    
+    print("Generating image...")
+    
+    let image = universe.cgImageRepresentation(using: colorMapping)!
+    
+    print("Saving image file...")
+    
+    let destination = CGImageDestinationCreateWithURL(outputUrl as CFURL, kUTTypePNG, 1, nil)!
+    CGImageDestinationAddImage(destination, image, nil)
+    CGImageDestinationFinalize(destination)
 }
 
-print("Instantiating view...")
-
-let view = UniverseView<MyDelegate>(frame: CGRect(x: 0, y: 0, width: 4000, height: 4000))
-view.universe = universe
-view.delegate = MyDelegate()
-
-do {
-    print("Generating output file...")
-    try view.dataWithPDF(inside: view.bounds).write(to: URL(fileURLWithPath: "/Users/CC/Desktop/CCA Outputs/Output.pdf"))
-} catch {
-    print(error)
-}
+main()
